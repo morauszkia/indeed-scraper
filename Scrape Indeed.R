35915ab6104830420a48ae20ab1dd8a280d2a759
Ketevani Kapanadze <56413302+kketevani@users.noreply.github.com>
2023-07-26 08:45:57 +0200

    Add files via upload

---
+++ Scrape Indeed.R
@@ -0,0 +1,138 @@
+# Kapanadze_Indeed Scraping 
+
+#Load libraries
+library(RSelenium)
+library(wdman)
+library(openxlsx)
+library(dplyr)
+
+#Launching an automated web
+port <- as.integer(4444L + rpois(lambda = 1000, 1))
+pJS <- wdman::chrome(port = port)
+remDrPJS <- remoteDriver(browserName = "chrome", port = port)
+remDrPJS$open()
+#setwd("/Users/ketevanikapanadze/chromedriver_mac64")
+
+# Navigate countries websites 
+remDrPJS$navigate('https://cz.indeed.com/')
+# dont forget to accept cookies (if any)
+#remDrPJS$navigate('https://de.indeed.com/')
+#remDrPJS$navigate('https://nl.indeed.com/')
+#remDrPJS$navigate('https://be.indeed.com/')
+#remDrPJS$navigate('https://uk.indeed.com/')
+#remDrPJS$navigate('https://ie.indeed.com/')
+#remDrPJS$navigate('https://es.indeed.com/')
+#remDrPJS$navigate('https://hu.indeed.com/')
+#remDrPJS$navigate('https://ch.indeed.com/')
+
+# Click and check the search-box
+checkbox <- remDrPJS$findElement(using = 'xpath', "//input[@id='text-input-what']")
+#checkbox$clickElement()
+#checkbox$clearElement()
+
+# Navigate occupations 
+checkbox$sendKeysToElement(list('kuchař',key='enter'))
+#checkbox$sendKeysToElement(list('Köche',key='enter'))
+
+# MAIN CODE
+########################################################################################################
+########################################################################################################
+# Avoid interacting with automated web while Selenium is running.......
+########################################################################################################
+########################################################################################################
+# Function to find element and return text or NA in case of an error
+getElementTextOrNA <- function(remDrPJS, xpath_expression) {
+  tryCatch({
+    element <- remDrPJS$findElement("xpath", xpath_expression)
+    if (is.null(element)) {
+      return(NA)
+    } else {
+      return(element$getElementText())
+    }
+  }, error = function(e) {
+    return(NA)
+  }, silent = TRUE)
+}
+
+# Initialize vectors to store location, company name, occupation, description, rating, salary and date
+location <- character()
+company <- character()
+occupation <- character()
+desc <- character()
+rating <- character()
+salary <- character()
+date <- character()
+
+while (TRUE) {
+  for (i in 1:15) {
+    # Use sprintf to create a dynamic XPath expression with index i 
+    xpath_expression_company <- sprintf("(//span[contains(@class, 'companyName')])[%d]", i)
+    company_data <- getElementTextOrNA(remDrPJS, xpath_expression_company)
+    company <- c(company, company_data)
+    
+    xpath_expression_rating <- sprintf("(//span[contains(@class, 'ratingNumber')])[%d]", i)
+    rating_data <- getElementTextOrNA(remDrPJS, xpath_expression_rating)
+    rating <- c(rating, rating_data)
+    
+    xpath_expression_location <- sprintf("(//div[contains(@class, 'companyLocation')])[%d]", i)
+    location_data <- getElementTextOrNA(remDrPJS, xpath_expression_location)
+    location <- c(location, location_data)
+    
+    xpath_expression_occupation <- sprintf("(//div[contains(@class, 'css-1m4cuuf e37uo190')])[%d]", i)
+    occupation_data <- getElementTextOrNA(remDrPJS, xpath_expression_occupation)
+    occupation <- c(occupation, occupation_data)
+    
+    xpath_expression_desc <- sprintf("(//div[contains(@class, 'job-snippet')])[%d]", i)
+    desc_data <- getElementTextOrNA(remDrPJS, xpath_expression_desc)
+    desc <- c(desc, desc_data)
+    
+    xpath_expression_salary <- sprintf("(//div[contains(@class, 'heading6 tapItem-gutter metadataContainer noJEMChips salaryOnly')])[%d]", i)
+    salary_data <- getElementTextOrNA(remDrPJS, xpath_expression_salary)
+    salary <- c(salary, salary_data)
+    
+    xpath_expression_date <- sprintf("(//span[contains(@class, 'date')])[%d]", i)
+    date_data <- getElementTextOrNA(remDrPJS, xpath_expression_date)
+    date <- c(date, date_data)
+  }
+  
+  #click next page
+  remDrPJS$findElement(using='xpath','//a[@data-testid="pagination-page-next"]')$clickElement()
+  
+  #sleep for a short while to allow the new page to load
+  Sys.sleep(1)
+}
+
+
+########################################################################################################
+########################################################################################################
+# Avoid interacting with automated web while Selenium is running.......
+########################################################################################################
+########################################################################################################
+
+#Merge/clean data
+location<-do.call(rbind.data.frame, location)
+colnames(location) <- c("location")
+company<-do.call(rbind.data.frame, company)
+colnames(company) <- c("company")
+occupation<-do.call(rbind.data.frame, occupation)
+colnames(occupation) <- c("occupation")
+desc<-do.call(rbind.data.frame, desc)
+colnames(desc) <- c("desc")
+rating<-do.call(rbind.data.frame, rating)
+colnames(rating) <- c("rating")
+salary<-do.call(rbind.data.frame, salary)
+colnames(salary) <- c("salary")
+date<-do.call(rbind.data.frame, date)
+colnames(date) <- c("date")
+
+# Final data 
+all <-cbind(location, company, occupation, desc, rating, salary, date)
+write.xlsx(all, file = "cook_cz.xlsx", rowNames = FALSE)
+
+
+
+#NOTE:  
+# In the main code, I pass the remDr client and the corresponding xpath_expression 
+# for each element to the getElementTextOrNA function. This will ensure that 
+# we handle errors properly and return "NA" when an element is not found or any other error occurs.
+# for more details, please see Indeed Scraping_readme file 
